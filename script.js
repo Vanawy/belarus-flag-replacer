56fe52118060820e8ad1159f70f210f1794471ab
Ilya Sakharchuk <saharchuk999@gmail.com>
2022-03-22 10:39:23 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Belarus Flag Replacer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Vanawy
+// @match        https://trackmania.io/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=trackmania.io
+// @grant        none
+// @source       https://github.com/Vanawy/belarus-flag-replacer
+// ==/UserScript==
+
+(new MutationObserver(check)).observe(document, {childList: true, subtree: true});
+
+function check(changes, observer) {
+    if(document.querySelector('[src="/img/flags/BLR.jpg"]')) {
+        observer.disconnect();
+        main();
+    }
+}
+
+function main() {
+    let flags = document.querySelectorAll('[src="/img/flags/BLR.jpg"]');
+    for (let flag of flags) {
+        flag.src = 'https://i.imgur.com/pyPe1V9.png';
+    }
+}
